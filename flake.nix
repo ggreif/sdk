fe29b15c725c0bfbbe02442ef903a5d18a5c1aae
Gabor Greif <gabor@dfinity.org>
2025-03-30 03:39:41 +0200

    WIP: specify `default`

--- flake.nix
+++ flake.nix
@@ -27,6 +27,24 @@
       ];
 
       CXXFLAGS_aarch64_apple_darwin = "--target=aarch64-apple-darwin";
+
+    };
+
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
+      pname = "dfx";
+      version = "0.25.1";
+      cargoLock.lockFile = ./Cargo.lock;
+      src = pkgs.lib.cleanSource ./.;
+      cargoLock = {
+        outputHashes = {
+          "derive_more-0.99.8-alpha.0" = "sha256-tEsfYC9oCAsDjinCsUDgRg3q6ruvayuA1lRmsEP9cys=";
+          "ic-agent-0.39.3" = "sha256-klkObvVY4GpIUYcN9lAs/cQT/az2TP2VdY8t4duGOGc=";
+          "ic-base-types-0.9.0" = "sha256-mM1FekwR0ZEyFTgs8aeN4kReksBSg8qcxK9wIEBv3vo=";
+          "ic-btc-interface-0.1.0" = "sha256-BbFLvf9TKWxqVnzjFPEQZ6Vl45/Vbq7592B3ywqeyI0=";
+          "ic-certification-2.3.0" = "sha256-nzJY2WLWIfRKNTReXYVTlxfRB9AS5oTyZ372kkKJLAg=";
+          "pocket-ic-7.0.0" = "sha256-ECYsWivtd1as6ZRfjiRfv2v0q0amj1QZibl5U8v83Mg=";
+        };
+      };
     };
   };
 }
